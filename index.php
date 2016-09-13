7fe64cdaefdf95598fbd1ba41b11e53f809656e4
Kevin Dilts <kdilts13@gmail.com>
2016-09-13 16:10:30 -0600

    Continuing to work on buttons.

--- index.php
+++ index.php
@@ -10,27 +10,27 @@
 			<textarea readonly cols="4" rows="1"></textarea>
 			<div>
 				<div>
-					<div class="button"></div>
-					<div class="button"></div>
-					<div class="button"></div>
+					<div class="button">7</div>
+					<div class="button">8</div>
+					<div class="button">9</div>
 				</div>
 
 				<div>
-					<div class="button"></div>
-					<div class="button"></div>
-					<div class="button"></div>
+					<div class="button">4</div>
+					<div class="button">5</div>
+					<div class="button">6</div>
 				</div>
 
 				<div>
-					<div class="button"></div>
-					<div class="button"></div>
-					<div class="button"></div>
+					<div class="button">1</div>
+					<div class="button">2</div>
+					<div class="button">3</div>
 				</div>
 
 				<div>
-					<div class="button"></div>
-					<div class="button"></div>
-					<div class="button"></div>
+					<div class="button">0</div>
+					<div class="button">C</div>
+					<div class="button">E</div>
 				</div>
 			</div>
 		</div>
