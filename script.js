4d504416b3cd705f90fc2907fd6b9afe49c0c1e2
Kevin Dilts <kdilts13@gmail.com>
2016-09-13 18:31:21 -0600

    All of the buttons have actions. The game can be won / lost.

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 window.onload = function(){
 
+	var randomNumber = "";
+	for(var i = 0; i < 4; i++){
+		randomNumber += Math.floor(Math.random()*9);
+	}
+
+	console.log(randomNumber);
+
 	var buttons = document.getElementsByClassName("button");
 	var guessBox = document.getElementById("guess");
 
 	for(var i in buttons) {
 		if(buttons[i].value !== "E" && buttons[i].value !=="C") {
 			buttons[i].onclick = function() {
-				guessBox.value += "" + this.value;
+				if(guessBox.value.length < 4) {
+					guessBox.value += "" + this.value;
+				}
 			}
 		}else if(buttons[i].value === "C"){
 			buttons[i].onclick = function(){
 				guessBox.value = '';
 			}
 		}else if(buttons[i].value === "E"){
-			// TODO
+			buttons[i].onclick = function() {
+				if(guessBox.value === randomNumber) {
+					console.log("You Win!");
+					guessBox.value = "You Win!";
+				}else{
+					console.log("Wrong!");
+					guessBox.value = "Wrong!";
+				}
+			}
 		}
 	}
 };
